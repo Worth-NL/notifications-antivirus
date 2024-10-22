e8569ec8e7e202851ba67998bc0a1746258a439d
UltimatePancake <pier.gaetani@gmail.com>
2024-10-22 15:09:14 +0200

    add logging

--- app/clamav_client.py
+++ app/clamav_client.py
@@ -7,29 +7,35 @@ AV_MODE_SOCKET = "SOCKET"
 
 class ClamavClient:
     def ___init__(self):
+        current_app.logger.debug("ClamAV client initialized")
         self.mode = current_app.config.get("ANTIVIRUS_MODE")
         self.host = current_app.config.get("ANTIVIRUS_HOST")
         self.port = current_app.config.get("ANTIVIRUS_PORT")
+        current_app.logger.debug("ClamAV client initialized")
 
     def get_connection(self):
         cd = clamd.ClamdUnixSocket()
 
         if self.mode is AV_MODE_NETWORK and self.host is not None and self.port is not None:
+            current_app.logger.debug("ClamAV client set to NETWORK mode")
             cd = clamd.ClamdNetworkSocket(host=self.host, port=self.port)
 
         return cd
 
     def ping(self):
+        current_app.logger.debug("ping")
         cd = self.get_connection()
 
         try:
             cd.ping()
-        except clamd.ClamdError:
+        except clamd.ClamdError as err:
+            current_app.logger.error("ClamAV error :: %s", err)
             return False
 
         return True
 
     def scan(self, stream):
+        current_app.logger.debug("scan")
         cd = self.get_connection()
         result = cd.instream(stream)
 
--- app/views.py
+++ app/views.py
@@ -12,6 +12,7 @@ cli = ClamavClient()
 
 @main_blueprint.route("/_status")
 def status():
+    current_app.logger.debug("/_status")
     if cli.ping():
         return "ok", 200
     else:
@@ -20,15 +21,20 @@ def status():
 
 @auth.verify_token
 def verify_token(token):
+    current_app.logger.debug("Token verification")
     return token == current_app.config["ANTIVIRUS_API_KEY"]
 
 
 @main_blueprint.route("/scan", methods=["POST"])
 @auth.login_required
 def scan_document():
+    current_app.logger.debug("/scan")
     if "document" not in request.files:
         return jsonify(error="No document upload"), 400
 
     result = cli.scan(request.files["document"])
+    response = jsonify(ok=result)
 
-    return jsonify(ok=result)
+    current_app.logger.debug(response)
+
+    return response
