ed70a5fb90494b885338fe0e62d51e67ab68ca7b
Pier-Angelo Gaetani <pier.gaetani@gmail.com>
2024-10-28 12:43:41 +0100

    improved logging

--- app/clamav_client.py
+++ app/clamav_client.py
@@ -7,23 +7,23 @@ AV_MODE_SOCKET = "SOCKET"
 
 class ClamavClient:
     def ___init__(self):
-        current_app.logger.debug("ClamAV client initialized")
+        current_app.logger.info("ClamAV client starting...")
         self.mode = current_app.config.get("ANTIVIRUS_MODE")
         self.host = current_app.config.get("ANTIVIRUS_HOST")
         self.port = current_app.config.get("ANTIVIRUS_PORT")
-        current_app.logger.debug("ClamAV client initialized")
+        current_app.logger.info("ClamAV client initialized")
 
     def get_connection(self):
         cd = clamd.ClamdUnixSocket()
 
         if self.mode is AV_MODE_NETWORK and self.host is not None and self.port is not None:
-            current_app.logger.debug("ClamAV client set to NETWORK mode")
+            current_app.logger.info("ClamAV client set to NETWORK mode")
             cd = clamd.ClamdNetworkSocket(host=self.host, port=self.port)
 
         return cd
 
     def ping(self):
-        current_app.logger.debug("ping")
+        current_app.logger.info("ping")
         cd = self.get_connection()
 
         try:
@@ -35,7 +35,7 @@ class ClamavClient:
         return True
 
     def scan(self, stream):
-        current_app.logger.debug("scan")
+        current_app.logger.info("scan")
         cd = self.get_connection()
         result = cd.instream(stream)
 
@@ -50,9 +50,11 @@ def clamav_scan(stream):
     cd = clamd.ClamdUnixSocket()
 
     av_mode = current_app.config.get("ANTIVIRUS_MODE")
+    current_app.logger.info("AV_MODE :: %s", av_mode)
     if av_mode == "NETWORK":
         av_host = current_app.config.get("ANTIVIRUS_HOST")
         av_port = current_app.config.get("ANTIVIRUS_PORT")
+        current_app.logger.info("AV host :: %s:%s", av_host, av_port)
         cd = clamd.ClamdNetworkSocket(host=av_host, port=av_port)
 
     result = cd.instream(stream)
